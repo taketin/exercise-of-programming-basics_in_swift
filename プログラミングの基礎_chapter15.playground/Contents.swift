b18e885b8fc0c1c96500c8d3683b9e1e0b7fdb8b
Hidenori TAKESHITA <taketin@linecorp.com>
2020-12-29 22:50:42 +0900

    Fixed

--- プログラミングの基礎_chapter15.playground/Contents.swift
+++ プログラミングの基礎_chapter15.playground/Contents.swift
@@ -4,7 +4,7 @@ import XCTest
 class Chapter15 {
     func quickSort<T: Comparable>(lst: [T]) -> [T] {
         let take = { (separater: T, lst: [T], f: (T, T) -> Bool) -> [T] in
-            return lst.filter
+            return lst.filter { f($0, separater) }
         }
 
         let takeLess = { (separater: T, lst: [T]) -> [T] in
@@ -12,22 +12,14 @@ class Chapter15 {
         }
 
         let takeGreater = { (separater: T, lst: [T]) -> [T] in
-
+            return take(separater, lst, >)
         }
 
         return lst.count == 0
             ? []
-            : quickSort(lst: takeLess(separater: lst[0], lst: Array(lst.dropFirst())))
-            + [lst[0]]
-            + quickSort(lst: takeGreater(separater: lst[0], lst: Array(lst.dropFirst())))
-    }
-
-    func takeLess(separater: Int, lst: [Int]) -> [Int] {
-        return lst.filter { $0 < separater }.sorted()
-    }
-
-    func takeGreater(separater: Int, lst: [Int]) -> [Int] {
-        return lst.filter { $0 > separater }.sorted()
+            : quickSort(lst: takeLess(lst[0], Array(lst.dropFirst())))
+                + [lst[0]]
+                + quickSort(lst: takeGreater(lst[0], Array(lst.dropFirst())))
     }
 }
 
@@ -38,19 +30,6 @@ class Chapter15Test: XCTestCase {
         XCTAssertEqual(c.quickSort(lst: [4,6,1,10,3,5,11]), [1,3,4,5,6,10,11])
         XCTAssertEqual(c.quickSort(lst: [10,0]), [0,10])
     }
-
-    func testTakeLess() {
-        let c = Chapter15()
-
-        XCTAssertEqual(c.takeLess(separater: 5, lst: [4,1,7,6]), [1,4])
-    }
-
-    func testTakeGreater() {
-        let c = Chapter15()
-
-        XCTAssertEqual(c.takeGreater(separater: 5, lst: [4,1,7,6]), [6,7])
-    }
 }
 
 Chapter15Test.defaultTestSuite.run()
-
